d2566f79d5a43a283292ef27e8602db06cc8ce0b
Uditha99 <109640405+Uditha99@users.noreply.github.com>
2023-07-25 19:10:35 +0530

    Add Loading Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,11 +10,14 @@ import { DeleteComponent } from './components/delete/delete.component';
 import { NewComponent } from './components/new/new.component';
 import { UpdateComponent } from './components/update/update.component';
 import { FindComponent } from './components/find/find.component';
-import {HttpClientModule} from "@angular/common/http";
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 import {MatInputModule} from "@angular/material/input";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {MatSnackBar, MatSnackBarModule} from "@angular/material/snack-bar";
 import {MatIconModule} from "@angular/material/icon";
+import { LoadingComponent } from './components/loading/loading.component';
+import {MatProgressSpinner, MatProgressSpinnerModule} from "@angular/material/progress-spinner";
+import {HttpManagerInterceptor} from "./components/interceptor/http-manager.interceptor";
 
 @NgModule({
   declarations: [
@@ -23,21 +26,26 @@ import {MatIconModule} from "@angular/material/icon";
     DeleteComponent,
     NewComponent,
     UpdateComponent,
-    FindComponent
+    FindComponent,
+    LoadingComponent
   ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    AppRoutingModule,
-    MatButtonModule,
-    HttpClientModule,
-    MatInputModule,
-    FormsModule,
-    ReactiveFormsModule,
-    MatSnackBarModule,
-    MatIconModule
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        AppRoutingModule,
+        MatButtonModule,
+        HttpClientModule,
+        MatInputModule,
+        FormsModule,
+        ReactiveFormsModule,
+        MatSnackBarModule,
+        MatIconModule,
+        MatProgressSpinnerModule,
+
+    ],
+  providers: [
+    {provide:HTTP_INTERCEPTORS, useClass:HttpManagerInterceptor,multi:true}
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
